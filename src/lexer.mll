c06eecf4b06eae7965bba0aeea2a07923207c8a9
SHUNICHIRO ITO <shun1ro.0808@gmail.com>
2022-07-01 22:19:35 +0900

    resubmmit report2

--- src/lexer.mll
+++ src/lexer.mll
@@ -29,7 +29,7 @@ rule main = parse
 | "*" { Parser.MULT }
 | "<" { Parser.LT }
 | "=" { Parser.EQ } 
-| "->" { Parser.RARROW } (* New! *)
+| "->" { Parser.RARROW }
 | "&&" { Parser.AND } (*3.2.3 &&の実装*)
 | "||" { Parser.OR } (*3.2.3 ||の実装*)
 
--- src/syntax.ml
+++ src/syntax.ml
@@ -12,7 +12,7 @@ type exp =
   | LetExp of id * exp * exp
   | LetMultExp of (id * exp) * exp
   | FunExp of id * exp
-  | DFunExp of id * exp
+  | DFunExp of id * exp (*実装できなかった*)
   | AppExp of exp * exp
   | LetRecExp of id * id * exp * exp
 
