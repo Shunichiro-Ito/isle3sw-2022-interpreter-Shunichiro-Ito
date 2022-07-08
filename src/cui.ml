023c28360ec63f1cb9b7eb015c2b15ff579b8b8a
SHUNICHIRO ITO <shun1ro.0808@gmail.com>
2022-07-08 14:10:04 +0900

    report2 resubmmsion3

--- src/cui.ml
+++ src/cui.ml
@@ -10,7 +10,7 @@ let rec read_eval_print env =
   pp_val v;
   print_newline();
   read_eval_print newenv) (*3.2.2 Evalで発生したエラーをtryでキャッチする*) with 
-  Eval.Error e -> print_string "Eval.error";print_newline();read_eval_print env (*3.2.2 Evalでエラーが発生したらEval.errorと出力してインタプリタプロンプトに戻るようにする。*)
+  Eval.Error e -> print_string e;print_newline();read_eval_print env (*3.2.2 Evalでエラーが発生したら対応するエラーメッセージを出力してインタプリタプロンプトに戻るようにする。*)
   |Parser.Error -> print_string "Parser.error";print_newline();read_eval_print env (*3.2.2 Parserでエラーが発生したらEval.errorと出力してインタプリタプロンプトに戻るようにする。*)
   |_ -> print_string "Eval、Parser以外でのエラー";print_newline();read_eval_print env (*3.2.2 lexerなどでエラーが発生したらEval.errorと出力してインタプリタプロンプトに戻るようにする。*)
 
